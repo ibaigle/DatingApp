f50c372da90d895f752f05209eeb51dbebde24e7
Iban Iglesias <iban.iglesias@getronics.com>
2021-07-21 07:53:24 +0200

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
   model: any = {}
   //currentUser$: Observable<User> | undefined;
   //we use the pipe in async mode to automaticly unsubscribe if a nav.component.ts is no longer visible
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     //this.currentUser$ = this.accountService.currentUser$;
@@ -20,14 +22,16 @@ export class NavComponent implements OnInit {
 
   login():void {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
       
     },error =>{
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
   /*getCurrentUser(){
