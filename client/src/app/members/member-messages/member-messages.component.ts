11648ad8c4291b4eacfb79943b503876d2bd3b36
Iban Iglesias <iban.iglesias@getronics.com>
2021-08-19 15:08:50 +0200

    Applied fix

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -14,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input() messages: Message[]= [];
   @Input() username: string="";
   messageContent: string="";
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -21,11 +22,12 @@ export class MemberMessagesComponent implements OnInit {
     
   }
   sendMessage(){
+    this.loading= true;
       //we use then when we use promises
     this.messageService.sendMessage(this.username, this.messageContent).then(() =>{
       //this.messages.push(message);
       this.messageForm.reset();
-    })
+    }).finally(()=> this.loading=false)
   }
   
 
